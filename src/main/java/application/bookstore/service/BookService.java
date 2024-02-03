4653878941ca5e00f637e642ee914ec000cd266d
Yaroslav Radevych-Vynnytskyi <loghitechdf@gmail.com>
2024-02-03 16:26:50 +0200

    Created Book entity, BookRepository and BookService. Configured MySQL connection.

---
+++ src/main/java/application/bookstore/service/BookService.java
@@ -0,0 +1,10 @@
+package application.bookstore.service;
+
+import application.bookstore.model.Book;
+import java.util.List;
+
+public interface BookService {
+    Book save(Book book);
+
+    List<Book> findAll();
+}
