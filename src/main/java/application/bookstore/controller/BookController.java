c77c93dd028208152d2e321bc8e966461f84d440
Yaroslav Radevych-Vynnytskyi <loghitechdf@gmail.com>
2024-02-08 20:08:55 +0200

    Added Liquibase support, replaced existing BookRepository with the one that uses JpaRepository. Added update by id and delete by id endpoints.

--- src/main/java/application/bookstore/controller/BookController.java
+++ src/main/java/application/bookstore/controller/BookController.java
@@ -6,11 +6,15 @@ import application.bookstore.service.BookService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -35,4 +39,14 @@ public class BookController {
         return bookService.save(bookDto);
     }
 
+    @PutMapping("/{id}")
+    public BookDto updateBook(@RequestBody CreateBookRequestDto bookDto, @PathVariable Long id) {
+        return bookService.updateBook(bookDto, id);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable Long id) {
+        bookService.deleteById(id);
+    }
 }
--- src/main/java/application/bookstore/service/impl/BookServiceImpl.java
+++ src/main/java/application/bookstore/service/impl/BookServiceImpl.java
@@ -2,6 +2,7 @@ package application.bookstore.service.impl;
 
 import application.bookstore.dto.BookDto;
 import application.bookstore.dto.CreateBookRequestDto;
+import application.bookstore.exception.EntityNotFoundException;
 import application.bookstore.mapper.BookMapper;
 import application.bookstore.model.Book;
 import application.bookstore.repository.BookRepository;
@@ -31,7 +32,26 @@ public class BookServiceImpl implements BookService {
 
     @Override
     public BookDto getBookById(Long id) {
-        Book book = bookRepository.getBookById(id);
+        Book book = bookRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Can't find book by id: " + id));
         return bookMapper.toDto(book);
+    }
+
+    @Override
+    public BookDto updateBook(CreateBookRequestDto bookDto, Long id) {
+        if (!bookRepository.existsById(id)) {
+            throw new EntityNotFoundException("Book with id: " + id + " does not exist");
+        }
+        Book book = bookMapper.toModel(bookDto);
+        book.setId(id);
+        return bookMapper.toDto(bookRepository.save(book));
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        if (!bookRepository.existsById(id)) {
+            throw new EntityNotFoundException("Book with id: " + id + " does not exist");
+        }
+        bookRepository.deleteById(id);
     }
 }
