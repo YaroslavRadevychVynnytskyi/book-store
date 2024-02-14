9c913ee6186ad7d7ba3468d528a81d1d2248cd6a
Yaroslav Radevych-Vynnytskyi <loghitechdf@gmail.com>
2024-02-14 10:43:46 +0200

    Added ability to search books by parameters

--- src/main/java/application/bookstore/repository/BookRepository.java
+++ src/main/java/application/bookstore/repository/BookRepository.java
@@ -3,5 +3,5 @@ package application.bookstore.repository;
 import application.bookstore.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookRepository extends JpaRepository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book, Integer> {
 }
