f843af9dd1696bbe397316d222984d4763e58d3b
Yaroslav Radevych-Vynnytskyi <loghitechdf@gmail.com>
2024-02-14 10:23:52 +0200

    Added ability to search books by parameters

---
+++ src/main/java/application/bookstore/repository/SpecificationBuilder.java
@@ -0,0 +1,8 @@
+package application.bookstore.repository;
+
+import application.bookstore.dto.BookSearchParameters;
+import org.springframework.data.jpa.domain.Specification;
+
+public interface SpecificationBuilder<T> {
+    Specification<T> build(BookSearchParameters searchParameters);
+}
