63c4db32e2e723c6bf844c1646bbe700deb2ea4b
Yaroslav Radevych-Vynnytskyi <loghitechdf@gmail.com>
2024-02-15 20:49:22 +0200

    Avoided code duplication. Reused constants from SpecificationProvider classes.

--- src/main/java/application/bookstore/repository/book/BookSpecificationBuilder.java
+++ src/main/java/application/bookstore/repository/book/BookSpecificationBuilder.java
@@ -4,6 +4,8 @@ import application.bookstore.dto.BookSearchParameters;
 import application.bookstore.model.Book;
 import application.bookstore.repository.SpecificationBuilder;
 import application.bookstore.repository.SpecificationProviderManager;
+import application.bookstore.repository.book.spec.AuthorSpecificationProvider;
+import application.bookstore.repository.book.spec.TitleSpecificationProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Component;
@@ -11,8 +13,6 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 @Component
 public class BookSpecificationBuilder implements SpecificationBuilder<Book> {
-    private static final String TITLE = "title";
-    private static final String AUTHOR = "author";
 
     private final SpecificationProviderManager<Book> specificationProviderManager;
 
@@ -21,12 +21,12 @@ public class BookSpecificationBuilder implements SpecificationBuilder<Book> {
         Specification<Book> specification = Specification.where(null);
         if (searchParameters.titles() != null && searchParameters.titles().length > 0) {
             specification = specification.and(specificationProviderManager
-                    .getSpecificationProvider(TITLE)
+                    .getSpecificationProvider(new TitleSpecificationProvider().getKey())
                     .getSpecification(searchParameters.titles()));
         }
         if (searchParameters.authors() != null && searchParameters.authors().length > 0) {
             specification = specification.and(specificationProviderManager
-                    .getSpecificationProvider(AUTHOR)
+                    .getSpecificationProvider(new AuthorSpecificationProvider().getKey())
                     .getSpecification(searchParameters.authors()));
         }
         return specification;
