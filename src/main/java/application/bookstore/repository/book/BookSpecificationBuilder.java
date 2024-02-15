8d23590834c5ef74011df15c0fc53b0144684871
Yaroslav Radevych-Vynnytskyi <loghitechdf@gmail.com>
2024-02-15 14:24:02 +0200

    Deployed constants for keys literals

--- src/main/java/application/bookstore/repository/book/BookSpecificationBuilder.java
+++ src/main/java/application/bookstore/repository/book/BookSpecificationBuilder.java
@@ -11,6 +11,9 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 @Component
 public class BookSpecificationBuilder implements SpecificationBuilder<Book> {
+    private static final String TITLE = "title";
+    private static final String AUTHOR = "author";
+
     private final SpecificationProviderManager<Book> specificationProviderManager;
 
     @Override
@@ -18,12 +21,12 @@ public class BookSpecificationBuilder implements SpecificationBuilder<Book> {
         Specification<Book> specification = Specification.where(null);
         if (searchParameters.titles() != null && searchParameters.titles().length > 0) {
             specification = specification.and(specificationProviderManager
-                    .getSpecificationProvider("title")
+                    .getSpecificationProvider(TITLE)
                     .getSpecification(searchParameters.titles()));
         }
         if (searchParameters.authors() != null && searchParameters.authors().length > 0) {
             specification = specification.and(specificationProviderManager
-                    .getSpecificationProvider("author")
+                    .getSpecificationProvider(AUTHOR)
                     .getSpecification(searchParameters.authors()));
         }
         return specification;
