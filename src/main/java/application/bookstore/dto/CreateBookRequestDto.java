7d6beb2f68a153c2099e5d7fcd050daaf8cd75c7
Yaroslav Radevych-Vynnytskyi <loghitechdf@gmail.com>
2024-02-16 20:42:59 +0200

    Increased validation detailing by specifying minimal description text length.

--- src/main/java/application/bookstore/dto/CreateBookRequestDto.java
+++ src/main/java/application/bookstore/dto/CreateBookRequestDto.java
@@ -2,13 +2,14 @@ package application.bookstore.dto;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 public class CreateBookRequestDto {
     private static final long MIN_VALUE = 0;
-
+    private static final int MIN_DESCRIPTION_LENGTH = 25;
     @NotNull
     private String title;
     @NotNull
@@ -18,6 +19,7 @@ public class CreateBookRequestDto {
     @NotNull
     @Min(MIN_VALUE)
     private BigDecimal price;
+    @Size(min = MIN_DESCRIPTION_LENGTH)
     private String description;
     private String coverImage;
 }
