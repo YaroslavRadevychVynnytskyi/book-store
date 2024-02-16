a2abf5c808a7245605c416c2d78ad2a0a431e1c1
Yaroslav Radevych-Vynnytskyi <loghitechdf@gmail.com>
2024-02-16 12:07:26 +0200

    Added CustomGlobalExceptionHandler class and validation for existing DTO classes

--- src/main/java/application/bookstore/dto/CreateBookRequestDto.java
+++ src/main/java/application/bookstore/dto/CreateBookRequestDto.java
@@ -1,13 +1,22 @@
 package application.bookstore.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 public class CreateBookRequestDto {
+    private static final long MIN_VALUE = 0;
+
+    @NotNull
     private String title;
+    @NotNull
     private String author;
+    @NotNull
     private String isbn;
+    @NotNull
+    @Min(MIN_VALUE)
     private BigDecimal price;
     private String description;
     private String coverImage;
--- src/main/java/application/bookstore/repository/book/BookSpecificationBuilder.java
+++ src/main/java/application/bookstore/repository/book/BookSpecificationBuilder.java
@@ -4,6 +4,8 @@ import application.bookstore.dto.BookSearchParameters;
 import application.bookstore.model.Book;
 import application.bookstore.repository.SpecificationBuilder;
 import application.bookstore.repository.SpecificationProviderManager;
+import application.bookstore.repository.book.spec.AuthorSpecificationProvider;
+import application.bookstore.repository.book.spec.TitleSpecificationProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Component;
@@ -18,12 +20,12 @@ public class BookSpecificationBuilder implements SpecificationBuilder<Book> {
         Specification<Book> specification = Specification.where(null);
         if (searchParameters.titles() != null && searchParameters.titles().length > 0) {
             specification = specification.and(specificationProviderManager
-                    .getSpecificationProvider("title")
+                    .getSpecificationProvider(new TitleSpecificationProvider().getKey())
                     .getSpecification(searchParameters.titles()));
         }
         if (searchParameters.authors() != null && searchParameters.authors().length > 0) {
             specification = specification.and(specificationProviderManager
-                    .getSpecificationProvider("author")
+                    .getSpecificationProvider(new AuthorSpecificationProvider().getKey())
                     .getSpecification(searchParameters.authors()));
         }
         return specification;
