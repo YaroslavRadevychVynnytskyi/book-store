ed4c1ec83f2f7712eee7042e920812e6eac5a464
Yaroslav Radevych-Vynnytskyi <loghitechdf@gmail.com>
2024-03-24 20:34:11 +0200

    Fixed bugs

--- src/main/java/application/bookstore/repository/shopping/cart/CartItemRepository.java
+++ src/main/java/application/bookstore/repository/shopping/cart/CartItemRepository.java
@@ -1,6 +1,7 @@
 package application.bookstore.repository.shopping.cart;
 
 import application.bookstore.model.CartItem;
+import java.util.Set;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -17,4 +18,9 @@ public interface CartItemRepository extends JpaRepository<CartItem, Long> {
             + "SET item.deleted = true "
             + "WHERE item.shoppingCart.id = :shoppingCartId")
     void deleteCartItemsByShoppingCartId(Long shoppingCartId);
+
+    @Query("SELECT item FROM CartItem item "
+            + "LEFT JOIN FETCH item.book "
+            + "WHERE item.shoppingCart.id = :shoppingCartId")
+    Set<CartItem> findByShoppingCartId(Long shoppingCartId);
 }
